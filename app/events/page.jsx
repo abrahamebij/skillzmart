b3b937adf6831943628afaa87027e078319f6711
ksohlo <alabssouls@gmail.com>
2024-11-14 19:25:32 +0100

    evebt detail page

---
+++ app/events/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-[calc(100vh_-_64px)] bg-slate-50'>
+        page
+    </div>
+  )
+}
+
+export default page
